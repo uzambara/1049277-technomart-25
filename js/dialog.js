1b2dd665a9c3c64e24e41700bf31915c49d2deb7
egor.simonov <egor-sima@mail.ru>
2019-06-07 23:11:04 +0500

    Добавил класс Dialog - Для управления диалоговыми окнами. Добавил показ и скрытие форм по нажатию на соответствующие интерактивные элементы.

---
+++ js/dialog.js
@@ -0,0 +1,78 @@
+function Dialog(dialogSelector, openElementSelector, closeElementSelector){
+    let that = this;
+    that.dialogSelector = dialogSelector;
+    that.dialogElement = document.querySelector(dialogSelector);
+
+    // Подписка для открытие формы
+    that.addOpenEventListener = function(){
+        let openElement = document.querySelector(openElementSelector);
+
+        if(openElement){
+            openElement.addEventListener("click", that.showDialog);
+        }else{
+            that.logBadSelectorError(openElementSelector);
+        }
+    }
+
+    // Подписка для открытие формы
+    that.addCloseEventListener = function(){
+        let closeElement = document.querySelector(closeElementSelector);
+
+        if(closeElement){
+            closeElement.addEventListener("click", that.closeDialog);
+        }
+        else{
+            that.logBadSelectorError(closeElementSelector);
+        }
+    }
+
+    that.showDialog = function(){
+        that.preventDefault(arguments);
+
+        if(that.dialogElement){
+            that.showOverlay();
+            that.dialogElement.classList.add(showDialogClassName);
+        }
+        else{
+            that.logBadSelectorError(dialogSelector);
+        }
+    }
+
+    that.closeDialog = function(){
+        that.preventDefault(arguments);
+
+        if(that.dialogElement){
+            that.closeOverlay();
+            that.dialogElement.classList.remove(showDialogClassName);
+        }
+        else{
+            that.logBadSelectorError(dialogSelector);
+        }
+    }
+
+    that.preventDefault = function(args){
+        if(args.length > 0){
+            let ev = args[0];
+            if(ev["preventDefault"])
+                ev.preventDefault();
+        }
+    }
+
+    that.showOverlay = function(){
+        let overlay = document.querySelector(overlaySelector);
+
+        if(overlay)
+            overlay.classList.add(showDialogClassName);
+    }
+
+    that.closeOverlay = function(){
+        let overlay = document.querySelector(overlaySelector);
+
+        if(overlay)
+            overlay.classList.remove(showDialogClassName);
+    }
+
+    that.logBadSelectorError = function(selectorValue){
+        console.error("Не удалось найти элемент по селектору: " + selectorValue);
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-    
+// Форма обратной связи
+const feedbackDialogSelector = ".dialog-form-feedback";
+const feedbackOpenSelector = ".button-write-us";
+const feedBackCloseSelector = ".dialog-form-feedback .close-button";
+
+// Карта
+const mapDialogSelector = ".dialog-map";
+const mapOpenSelector = ".company-info-map-link";
+const mapCloseSelector = ".dialog-map .close-button"
+
+const showDialogClassName = "show-dialog";
+const overlaySelector = ".overlay";
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Форма обратной связи
+    let writeUsDialog = new Dialog(
+        dialogSelector = feedbackDialogSelector,
+        openElementSelector = feedbackOpenSelector,
+        closeElementSelector = feedBackCloseSelector);
+
+    writeUsDialog.addOpenEventListener();
+    writeUsDialog.addCloseEventListener();
+
+    // Карта
+    let mapDialog = new Dialog(
+        dialogSelector = mapDialogSelector,
+        openElementSelector = mapOpenSelector,
+        closeElementSelector = mapCloseSelector);
+
+    mapDialog.addOpenEventListener();
+    mapDialog.addCloseEventListener();
 });
