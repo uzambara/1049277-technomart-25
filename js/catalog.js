384de10f2b498b5b3f62f10eb28bb08867981622
egor.simonov <egor-sima@mail.ru>
2019-06-11 01:51:11 +0500

    Сделал выполнение скрипта после загрузки

--- js/catalog.js
+++ js/catalog.js
@@ -3,9 +3,11 @@ const SUCCESS_CART_ADDING_OPEN_SELECTOR = ".product-card-actions-buy";
 const SUCCESS_CART_ADDING_CLOSE_SELECTOR = ".dialog-form-buy-success .close-button";
 const CONTINUE_SHOPPING_SELECTOR = ".button.button-continue-shopping";
 
-let successCartAddingDialog = new Dialog(
-    SUCCESS_CART_ADDING_DIALOG_SELECTOR);
+document.addEventListener("DOMContentLoaded", function () {
+    let successCartAddingDialog = new Dialog(
+        SUCCESS_CART_ADDING_DIALOG_SELECTOR);
 
-successCartAddingDialog.addOpenEventListener(SUCCESS_CART_ADDING_OPEN_SELECTOR);
-successCartAddingDialog.addCloseEventListener(SUCCESS_CART_ADDING_CLOSE_SELECTOR);
-successCartAddingDialog.addCloseEventListener(CONTINUE_SHOPPING_SELECTOR);
+    successCartAddingDialog.addOpenEventListener(SUCCESS_CART_ADDING_OPEN_SELECTOR);
+    successCartAddingDialog.addCloseEventListener(SUCCESS_CART_ADDING_CLOSE_SELECTOR);
+    successCartAddingDialog.addCloseEventListener(CONTINUE_SHOPPING_SELECTOR);
+});
